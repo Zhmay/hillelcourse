6db4415d5e26636aad2b6e4088426f755ed127d2
bogdan <bogdanzhmay@gmail.com>
2021-10-12 16:24:21 +0300

    home work 4

--- index.php
+++ index.php
@@ -1,2 +1,75 @@
 <?php
+$arr = [12, [10, 5], 5, 'five' => ['one' => 30,'two' => 5,'three' => [10, 9]], 8];
 
+//1. Создать функцию принимающую массив произвольной вложенности и определяющий любой элемент номер которого передан параметром во всех вложенных массивах.
+function getElement(array $array, $number) {
+    $result = [];
+    $key = 0;
+
+    foreach ($array as $elem) {
+        if($key == $number) {
+            $result[] = $elem;
+            if(is_array($elem)) {
+                $newResult = getElement($elem, $number);
+                foreach($newResult as $value){
+                    $result[] = $value;
+                }
+            }
+        } else {
+            if(is_array($elem)) {
+                $newResult = getElement($elem, $number);
+                foreach($newResult as $value){
+                    $result[] = $value;
+                }
+            }
+        }
+        $key++;
+    }
+
+    return $result;
+    echo '<br>';
+}
+print_r(getElement($arr, 1));
+
+
+
+
+//2. Создать функцию которая считает все буквы b в переданной строке, в случае если передается не строка функция должна возвращать false
+function getLetterCount($string, $letter) {
+    if(is_string($string)) {
+        echo substr_count($string, $letter) . '<br>';
+    } else {
+        return false;
+    }
+}
+getLetterCount('Создать функцию bb которая считает все буквы b в переданной строке', 'b');
+
+
+//3. Создать функцию которая считает сумму значений всех элементов массива произвольной глубины
+function getTotalSum(array $array=[]) {
+    $totalSum = array_sum($array);
+
+    foreach ($array as $value) {
+        if(is_array($value)) {
+            $totalSum += array_sum($value);
+        }
+    }
+    echo $totalSum . '<br>';
+}
+getTotalSum($arr);
+
+
+//4. Создать функцию которая определит сколько квадратов меньшего размера можно вписать в квадрат большего размера размер возвращать в float
+function squaring($sqrFirst, $sqrSecond) {
+
+    if ($sqrFirst < $sqrSecond) {
+        $res = ($sqrSecond * $sqrSecond) / ($sqrFirst * $sqrFirst);
+    } else {
+        $res = ($sqrFirst * $sqrFirst) / ($sqrSecond * $sqrSecond);
+    }
+
+    return $res;
+}
+echo squaring(2, 9) . '<br>';
+
+echo '<br>';
