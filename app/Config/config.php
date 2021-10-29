3fc6b64ffa4ff68ac5b96ec66b8c21e712d58e19
bogdan <bogdanzhmay@gmail.com>
2021-10-29 21:39:35 +0300

    lesson 11 Model

--- app/Config/config.php
+++ app/Config/config.php
@@ -7,6 +7,7 @@
         'App\Controller\Admin\Admin@index' => 'admin/index',
         'App\Controller\Admin\Admin@dashboard' => 'admin/dashboard',
         'App\Controller\Admin\Login@login' => 'admin/login',
+        'App\Controller\Admin\User@user' => 'admin/user',
         'App\Controller\Home\Home' => 'home',
         'App\Controller\Home\Home@index' => 'home/index',
         'App\Controller\Home\Home@view' => 'home/view',
---
+++ app/Controller/Admin/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller\Admin;
+
+class Post
+{
+    public $posts = [
+        "post1" => "hello",
+        "post2" => "some post 1",
+        "post3" => "some post 2",
+        "post4" => "some post 3",
+    ];
+}
