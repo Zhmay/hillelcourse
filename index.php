3d7778ccdb3d19dc5ba50976e1e30f2730eeb482
bogdan <bogdanzhmay@gmail.com>
2021-11-03 14:50:46 +0200

    home work 11

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
-    require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
-    use Core\Router;
+use Core\Router;
+use Lib\DB\Connector;
 
-    $router = new Router();
-    $router->run();
+$connector = new Connector();
+$con = $connector->getConnect();
+$add = $con->prepare("INSERT INTO users (name, age) VALUES ('Bogdan', 30)");
+$remove = $con->prepare("DELETE FROM posts WHERE (posts.id = 3)");
+$update = $con->prepare("UPDATE `posts` SET `detail` = 'update text here 1' WHERE `posts`.`id` = 3;");
+
+
+$select = $con->prepare('SELECT * FROM posts', array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+$select->execute();
+$posts = $select->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<pre>';
+print_r($posts);
+echo '</pre>';
+
+//$router = new Router();
+//$router->run();
